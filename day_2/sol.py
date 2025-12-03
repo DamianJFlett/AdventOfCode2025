f53e5109a70a9b4e8242c2d88eb6dff27cfdb92f
Damian Flett <damian.flett@outlook.com>
2025-12-03 15:49:34 +1000

    day 3 and day 2 submitted and complete

--- day_2/sol.py
+++ day_2/sol.py
@@ -5,27 +5,48 @@ def load_data():
     Load/preprocess data and returns what we need from it
     """
     input = open("input.txt", "r")
+    data = []
+    data2 = []
+    for l in input:
+        x = l[:-1]
+        for range in x.split(","):
+            data.append(tuple(range.split("-")))
+            data2+= [y for y in range.split("-")]
 
+    return data
 
-    return None
-
-def p1():
+def p1(data):
     """
     Part 1
     """
-    pass
+    data2 = []
+    for id1, id2 in data:
+        data2 += [str(i) for i in range(int(id1), int(id2))]
+    invalid_ids = [int(id) for id in data2 if id[:len(id)//2 ] == id[len(id) // 2 :] and (not (len(id) % 2)) and (len(id) >= 2)]
+    print(invalid_ids)
+    print(sum(invalid_ids))
 
 
-def p2():
+def p2(data):
     """
     Part 2
     """
-    pass
-
+    data2 = []
+    for id1, id2 in data:
+        data2 += [str(i) for i in range(int(id1), int(id2))]
+    invalid_ids = set()
+    for id in data2:
+        for modulo in range(2, len(id)+ 1):
+            if (not len(id) % modulo) and (len(id) >= modulo) and id == (id[:len(id) // modulo] * modulo):
+                invalid_ids.add(int(id))
+                break
+    print(invalid_ids)
+    print(sum(invalid_ids))
 
 def main():
-    p1()
-    #p2()
+    data = load_data()
+    #p1(data)
+    p2(data)
 
 
 if __name__ == "__main__":
--- day_3/sol.py
+++ day_3/sol.py
@@ -5,27 +5,55 @@ def load_data():
     Load/preprocess data and returns what we need from it
     """
     input = open("input.txt", "r")
+    data = []
+    for l in input:
+        data.append(list(int(x) for x in l[:-1]))
+    return data
 
-
-    return None
-
-def p1():
+def p1(data):
     """
-    Part 1
+    Part 1 
     """
-    pass
+    total = 0
+    for row in data:
+        maxie = -1
+        maxpos = -1
+        for index, num in enumerate(row[:-1]):
+            if num > maxie:
+                maxie = num
+                maxpos = index
+        num2 = max([int(x) for x in row[maxpos+1:]])
+        total += 10*maxie + num2
+    print(total)
 
 
-def p2():
+def p2(data):
     """
     Part 2
     """
-    pass
+    total = 0
+    for row in data:
+        digits = []
+        old_pos = -1
+        for i in range(12, 0, -1):
+            max_dig = -1
+            for index, num in enumerate(row[old_pos + 1:len(row) - (i - 1)]):
+                if num > max_dig:
+                    max_dig = num
+                    start_pos = old_pos+1+index
+            digits.append(max_dig)
+            old_pos = start_pos
+        num = sum(10**i * digits[11-i] for i in range(12))
+        print(num)
+        total += num
+    print(total)
+    
 
 
 def main():
-    p1()
-    #p2()
+    data = load_data()
+    p1(data)
+    p2(data)
 
 
 if __name__ == "__main__":
